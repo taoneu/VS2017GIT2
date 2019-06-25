134313385bd12709690ec4ec85f6eb7ef6c2b5cf
taoneu <329920283@qq.com>
2019-06-26 00:30:43 +0800

    第一个项目

---
+++ test1/test1/源.cpp
@@ -0,0 +1,18 @@
+#include "common.h" //引入共通的头文件
+
+
+/*
+使用VisualStudio2017开发的第一个C语言程序
+@author tony tonytimemachine@gmail.com
+@since 2016年12月26日
+*/
+void main() {
+
+	printf("Hello World in Windows10 With VisualStudio2017 Enterprise");
+	getchar(); //等待输入任意字符退出程序
+}
+//-------------------- -
+//作者：boy_cto_tony
+//来源：CSDN
+//原文：https ://blog.csdn.net/ITTechnologyHome/article/details/53891087 
+//版权声明：本文为博主原创文章，转载请附上博文链接！
